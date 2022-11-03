ee9e05be7785f47bc1e44442e40e6826678bd703
TableFlipGod <69564793+IAmTheTable@users.noreply.github.com>
2022-11-03 12:55:00 -0700

    Add project files.

---
+++ ConvertTohex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace ConvertTohex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertToHex("Hello, World!");
+        }
+
+        static void ConvertToHex(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            var hex = BitConverter.ToString(bytes).Replace("-", " ").ToLower();
+            Console.WriteLine(hex);
+        }
+    }
+}
